04edec63b361b3d61361aef1b9bbd1839e10ecbb
Dennis Webb <dennis@bluesentryit.com>
2019-04-02 21:00:55 -0500

    Sleep (#5)
    * feat(sleep): Adds sleep
    
    * Adds tests

--- src/bin/sleep/main.rs
+++ src/bin/sleep/main.rs
@@ -8,18 +8,21 @@ fn main() {
     }
 
     for arg in args().skip(1) {
-        let duration = match arg.parse::<f64>() {
-            Ok(seconds) => Duration::from_millis((seconds * 1000.0) as u64),
-            Err(_) => humantime::parse_duration(&arg).unwrap_or_default(),
-        };
-
-        sleep(duration);
+        sleep(str_to_duration(&arg));
     }
 }
 
-fn sleep(duration: Duration) -> () {
+fn sleep(duration: Duration) {
     if duration > Duration::from_millis(0) {
+        dbg!(duration);
         thread::sleep(duration)
+    }
+}
+
+fn str_to_duration(input: &str) -> Duration {
+    match input.parse::<f64>() {
+        Ok(seconds) => Duration::from_millis((seconds * 1000.0) as u64),
+        Err(_) => humantime::parse_duration(input).unwrap_or_default(),
     }
 }
 
@@ -35,4 +38,30 @@ examples: {cmd} 5
             .to_str()
             .unwrap()
     )
+}
+
+#[cfg(test)]
+mod should {
+    use super::*;
+    use std::time::Duration;
+
+    #[test]
+    fn convert_cmplex_string_to_duration() {
+        assert_eq!(Duration::from_secs((60*60*24) + (60*60*2) + (60*3) + 4), str_to_duration("1d2h3m4s"))
+    }
+
+    #[test]
+    fn convert_junk_string_to_0_seconds() {
+        assert_eq!(Duration::from_secs(0), str_to_duration("rustaceans"))
+    }
+
+    #[test]
+    fn convert_number_to_seconds() {
+        assert_eq!(Duration::from_secs(5), str_to_duration("5"))
+    }
+
+    #[test]
+    fn convert_string_to_duration() {
+        assert_eq!(Duration::from_secs(60), str_to_duration("1m"))
+    }
 }
